369a8169c5fec27c5c19e0ebdccdeffa1d8df559
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-07-28 18:31:32 -0500

    browserrouter up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router'
 import BasicRoutings from './components/BasicRoutings'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <BasicRoutings />
-    </BrowserRouter>
+    </>
   )
 }
