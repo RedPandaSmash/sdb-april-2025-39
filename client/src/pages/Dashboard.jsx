0c0adb346f1edc4be28d64f5d00989f502b80863
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-07-28 19:16:33 -0500

    working dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useNavigate } from 'react-router'
+
+
 
 export default function Dashboard() {
+    const [data, setData] = useState(null)
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        const token = localStorage.getItem("token")
+        if(!token){
+            navigate("/login")
+            return
+        }
+
+        fetch("http://localhost:8080/api/private", {
+            headers: {
+                Authorization: token
+            }
+        })
+        .then((res)=>{
+            if(res.status === 401){
+                navigate("/login")
+                return
+            }
+            return res.json()
+        })
+        .then((data)=>{
+            setData(data)
+        })
+        .catch((err)=>{
+            console.error(err)
+        })
+        .finally(()=>{
+            // cleaning up
+            console.log("Data:", data)
+        })
+    }, [])
+
+
   return (
-    <div>Dashboard</div>
+    <div>
+        <h1>Dashboard</h1>
+        <p>all the private information</p>
+    </div>
   )
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Login() {
+    const [form, setForm] = useState({
+        email: '',
+        password: '',
+    })
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = async (e) =>{
+        // prevent default form submission
+        e.preventDefault()
+
+        // send API request to login user/
+        const res = await fetch("http://localhost:8080/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(form)
+        })
+
+        const data = await res.json()
+
+        console.log(data)
+
+        if(data.token){
+            localStorage.setItem("token", data.token)
+        }
+
+        alert(data.message)
+
+        // reset form after submission
+        setForm({
+            email: '',  
+            password: '',
+        })
+    }
   return (
-    <div>Login</div>
+    <div>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+            <input type="email" name="email" placeholder="Email" value={form.email} onChange={handleChange} />
+            <input type="password" name="password" placeholder="Password" value={form.password} onChange={handleChange} />
+            <button type="submit">Login</button>
+        </form>
+    </div>
   )
 }
